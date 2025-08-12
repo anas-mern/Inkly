f0d50967798208f785cae3e7b6253761380a142b
fmip313frontend@gmail.com <fmip313frontend@gmail.com>
2025-08-13 01:08:05 +0300

    Solving Router Error

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/jobs-app">
     <App />
   </BrowserRouter>
 );
